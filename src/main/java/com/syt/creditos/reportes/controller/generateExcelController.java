33c5f2f797dfb32c320cb2712f2ebc54909ff959
javier-lopez <javier.lopez@bowpi.com>
2020-10-09 12:56:18 -0600

    EDIT REPORTE DE PAGOS EN EXCEL

--- src/main/java/com/syt/creditos/reportes/controller/generateExcelController.java
+++ src/main/java/com/syt/creditos/reportes/controller/generateExcelController.java
@@ -1,5 +1,6 @@
 package com.syt.creditos.reportes.controller;
 
+import com.syt.creditos.reportes.feingService.pago;
 import com.syt.creditos.reportes.service.ExcelReport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
@@ -7,8 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
 
 @RestController
 @CrossOrigin
@@ -18,10 +22,25 @@ public class generateExcelController {
     @Autowired
     private ExcelReport excelReport;
 
-    @PostMapping(value = "/excelexport")
-    public ResponseEntity<InputStreamResource> subVaribleExcel(/*@RequestBody SubVariable variable*/) throws IOException {
-        //List<SubVariableExcel> subVariableExcels = subVariableRepository.findAllForExcel(variable.getVariable_id());
-        ByteArrayInputStream in = excelReport.generarExcel();//SubVariableExcelReport.subVariableExcel(subVariableExcels);
+    @Autowired
+    private pago pagoFeing;
+
+    @PostMapping(value = "/pagos")
+    public ResponseEntity<InputStreamResource> subVaribleExcel(@Valid @RequestBody Map<String, Object> map) throws IOException {
+
+        ArrayList<Object> detallePago = pagoFeing.generarReporte(map);
+        ArrayList<String> array = new ArrayList<>();
+        array.add("Crédito");
+        array.add("PrimerNombre");
+        array.add("PrimerApellido");
+        array.add("Abono Capital");
+        array.add("Interés");
+        array.add("Cuota Seguro");
+        array.add("Total");
+        array.add("PagoExtraCapital");
+        array.add("FechaSistema");
+
+        ByteArrayInputStream in = excelReport.generarExcel(array, detallePago);
         // return IO ByteArray(in);
         HttpHeaders headers = new HttpHeaders();
         // set filename in header
--- src/main/java/com/syt/creditos/reportes/service/ExcelReport.java
+++ src/main/java/com/syt/creditos/reportes/service/ExcelReport.java
@@ -14,8 +14,8 @@ import java.util.List;
 @Component
 public class ExcelReport {
 
-    public static ByteArrayInputStream generarExcel(/*List<Object> datos*/) throws IOException {
-        int columns = 2;//datos.size();//para generar total de columnas por ID encontrados
+    public static ByteArrayInputStream generarExcel(ArrayList<String> titulos, ArrayList<Object> datos) throws IOException {
+
         try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
             Sheet sheet = workbook.createSheet("Datos");
 
@@ -30,55 +30,21 @@ public class ExcelReport {
             // Header Row
             Row headerRow = sheet.createRow(0);
 
-            Cell headerCell = headerRow.createCell(0);
-            headerCell.setCellValue("IdPrestamo");
-            //headerCell.setCellStyle(headerCellStyle);
-
-            headerCell = headerRow.createCell(1);
-            headerCell.setCellValue("Primer Nombre");
-            //headerCell.setCellStyle(headerCellStyle);
-            ArrayList<Object> array = new ArrayList<>();
-array.add(0, "Luis");
-            array.add(1, "Pedro");
-            //columns = array.size();
-/*
-            int col = 0;
-            for (Object dato : datos) {
-                Cell cell = headerRow.createCell(col);
-                cell.setCellValue("Name");
-                cell.setCellStyle(headerCellStyle);
-                col+=2;
-            }*/
-
-            Row headerRow2 = sheet.createRow(1);
-/*
-            Cell cell = headerRow2.createCell(0);
-            cell.setCellValue("John Smith");
-            //cell.setCellStyle(style);
-
-            cell = headerRow2.createCell(1);
-            cell.setCellValue(20);
-            //cell.setCellStyle(style);
-            */
-            for (int colx = 0; colx < columns*2; colx++) {
-                Cell cell = headerRow2.createCell(colx);
-                if(colx%2==0)
-                    cell.setCellValue(array.get(colx).toString());
-                else
-                    cell.setCellValue(array.get(colx).toString());
-
-                sheet.autoSizeColumn(colx);
+            for (int i = 0; i < titulos.size(); i ++) {
+                Cell headerCell = headerRow.createCell(i);
+                headerCell.setCellValue(titulos.get(i).toString());
             }
-/*
-            int firstCol = 0;
-            int lastCol = 1;
 
-            for (int colx = 0; colx < columns; colx++) {
-                sheet.addMergedRegion(new CellRangeAddress(0, 0, firstCol, lastCol));
-                firstCol+=2;
-                lastCol+=2;
-                //asi voy fucionando las columnas de 2 en 2 según numero de registro
-            }*/
+            for (int i = 0; i < datos.size(); i++) {
+                ArrayList list = (ArrayList) datos.get(i);
+                Row headerRow2 = sheet.createRow(i + 1);
+                for (int j = 0; j < titulos.size(); j++) {
+                    Cell cell = headerRow2.createCell(j);
+                    cell.setCellValue(list.get(j) == null ? "" : list.get(j).toString());
+                    sheet.autoSizeColumn(j);
+                }
+            }
+
             workbook.write(out);
             return new ByteArrayInputStream(out.toByteArray());
         }
